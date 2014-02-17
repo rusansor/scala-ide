628ca6f3313505ed12b53cb0c70e2a1d8d522a69
rusansor <rusansor@gmail.com>
2014-02-17 18:28:36 +0100

    Add more unit tests for the Indentation Logic.
    The Scala IDE ticket has been used to create the new unit tests
    (https://scala-ide-portfolio.assembla.com/spaces/scala-ide/tickets/).
    
    I have added the comment "@Simon" to request his help/hints at specific
    code lines.

--- org.scala-ide.sdt.core.tests/src/org/scalaide/core/ui/indentation/ClassIndentationTests.scala
+++ org.scala-ide.sdt.core.tests/src/org/scalaide/core/ui/indentation/ClassIndentationTests.scala
@@ -117,4 +117,92 @@ class ClassIndentationTests extends ScalaAutoIndentStrategyTest {
       ^with T
     """ after linebreak
   }
+
+  /*  @Simon: it passes the test BUT it does NOT work properly in my Scala IDE!!
+   *   My Scala IDE still suffers the issue as described in the ticket.
+   */
+  //  @Ignore("wrong indentation; incorrect (after val). See ticket #1000415")
+  @Test
+  def afterVal() {
+    """
+    val foo = "foo"^
+    """ becomes
+      """
+    val foo = "foo"
+    ^
+    """ after linebreak
+  }
+
+  /*@Simon: The ticket is marked as fixed. TBut this test fails. 
+  Although it works in my Scala IDE!! May it be a regression test?*/
+  @Ignore("incorrect indentation inside comments. See ticket #1001384.")
+  @Test
+  def comment() {
+    """
+    /**
+    *        hello^
+    *        
+    */
+    """ becomes
+      """
+    /**
+    *        hello
+    *        ^
+    *        
+    */
+    """ after linebreak
+  }
+
+  /*@Simon: It seems it is needed to extend ScalaAutoIndentStrategyTest to support "e" press hit.
+   *  Is that correct? If so I can give it a try.   
+   */
+  @Ignore("incorrect indentatio. See ticket #1001306")
+  @Test
+  def ifElseReIndentationForElse() {
+    """
+    if (true)
+    1
+    else
+    """ becomes
+      """
+    if (true)
+    1
+    els --> press e TODO handle "e" press hit.
+    """ after linebreak
+  }
+
+  @Ignore("""wrong indentation; triple quoted strings. See ticket #1001670. 
+      Alghouth currently it seems to work a little bit better than described in the ticket #1001670.""")
+  @Test
+  def afterTripleQuotes() {
+    """
+    val s = \\"\\"\\"a b^ c\\"\\"\\"
+    """ becomes
+      """
+    val s = \\"\\"\\"a b
+     ^c\\"\\"\\"
+    """ after linebreak
+  }
+
+  //@Simon: Do you think is it feasible to extend ScalaAutoIndentStrategyTest to support Ctrl + space + choose a template?
+  @Ignore("""wrong indentation; triple quoted strings. See ticket #1000626. 
+      to reproduce: inside an indented context (such as a method body) type a keyword with
+      a multiline expansion template (e.g. 'if') and hit ctrl-space and select the template to expand. 
+      Actual Result: the template expands, but every line after the first has an indent of 0.""")
+  @Test
+  def insideAnIndentedContext() {
+    """
+    def myFunc(x: Int):Int {
+    if^  //TODO Press Ctrl+pace+chooe "if" template.
+    }
+    """ becomes
+      """
+    def myFunc(x: Int):Int {
+      if (condition) {
+        ^ 
+      }
+    }
+    """ after linebreak
+  }
+
 }
